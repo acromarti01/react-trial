369ccefabccb60e94dce867938a88601042e5acb
Anthony Cromartie <acromarti@gmail.com>
2021-10-10 13:29:30 -0500

    added October

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Today October 10th
         </a>
       </header>
     </div>
